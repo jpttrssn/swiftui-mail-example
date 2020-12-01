1d23c4cf252411f3421b0c4902e0bb2c04cc1d35
Jonatan Pettersson <jonatan.pettersson@pm.me>
2020-12-01 19:56:37 +0100

    Initial Commit

---
+++ swiftui-mail-example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-mail-example
+//
+//  Created by Jon Pettersson on 2020-12-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-mail-example/swiftui_mail_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_mail_exampleApp.swift
+//  swiftui-mail-example
+//
+//  Created by Jon Pettersson on 2020-12-01.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_mail_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
